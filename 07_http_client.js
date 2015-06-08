0719b99774859fdc0ce1f7a894e1d1be9c24d0c9
8ENs <ben@coderush.ca>
2015-06-08 15:05:45 -0700

    7 cleaned up

--- 07_http_client.js
+++ 07_http_client.js
@@ -2,12 +2,7 @@ var http = require('http');
 var url = process.argv[2];
 
 http.get(url, function(response) {
-	var body = '';
-	response.on("data", function(data) { 
+	response.on('data', function(data) { 
 		console.log(data.toString());
 	});	
 });
-
-// function callback (response) { /* ... */ }
-// response.on("data", function (data) { /* ... */ })
-// .toString();
